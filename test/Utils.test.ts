1c4a4a457ebd77f6d73ebda1639fa9d10cb2c162
Pixel <louis.vincent.bompart@gmail.com>
2019-06-27 04:19:51 -0400

    test(Utils): Update UTs for isValidPlatform

--- test/Utils.test.ts
+++ test/Utils.test.ts
@@ -1,19 +1,22 @@
-import { isValidPlatform } from "../src/Utils";
+import { isPlatformSupported } from "../src/Utils";
 import { expect } from "chai";
+import { BungieMembershipType } from "bungie-api-ts/common";
 
 describe("Utils", () => {
+
   describe("#isValidPlatform", () => {
+    
     it("should return true for curated platforms", () => {
-      const curatedPlatform = ["pc", "xbox", "playstation"];
+      const curatedPlatform = [BungieMembershipType.TigerBlizzard, BungieMembershipType.TigerPsn, BungieMembershipType.TigerXbox]
       curatedPlatform.forEach(platform => {
-        expect(isValidPlatform(platform), `${platform} is not recognized`).to.be
+        expect(isPlatformSupported(platform), `${platform} is not recognized`).to.be
           .true;
       });
     });
 
     it("should return false for non-curated platforms", () => {
-      ["", "SNES"].forEach(platform => {
-        expect(isValidPlatform(platform), `${platform} is wrongly recognized`)
+      [BungieMembershipType.None, BungieMembershipType.BungieNext, BungieMembershipType.All].forEach(platform => {
+        expect(isPlatformSupported(platform), `${platform} is wrongly recognized`)
           .to.be.false;
       });
     });
