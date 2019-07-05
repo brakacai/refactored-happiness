11ab5fd768d76de4e8afe727326ecdb904a396d0
Akumati <migit128@gmail.com>
2019-07-04 19:43:58 -0700

    checks if destiny is running.  if not, clears the activity

--- src/Utils.ts
+++ src/Utils.ts
@@ -3,12 +3,13 @@ import { get } from "request-promise-native";
 import { System } from "./System";
 
 const BUNGIE_ENDPOINT = "https://www.bungie.net/Platform";
+const exec = require('child_process').exec
 /**
  * The param to send to @function getFromBungie to get some data.
  */
 export interface BungieAPIParams {
-  uri: string;
-  components?: DestinyComponentType | DestinyComponentType[];
+    uri: string;
+    components?: DestinyComponentType | DestinyComponentType[];
 }
 
 /**
@@ -17,26 +18,26 @@ export interface BungieAPIParams {
  * @param bungiekey the API Key to authorize the call
  */
 export async function getFromBungie<T>(params: BungieAPIParams, bungiekey: string): Promise<T> {
-  const url = new URL(`${BUNGIE_ENDPOINT}/${params.uri}`);
-  // Normalize components as an array.
-  if (params.components) {
-    params.components = Array.isArray(params.components) ? params.components : [params.components];
-    url.searchParams.set("components", params.components.join(","));
-  }
-  try {
-    return (await get({ uri: url.toString(), headers: { "X-API-Key": bungiekey }, json: true })) as T;
-  } catch (error) {
-    return error;
-  }
+    const url = new URL(`${BUNGIE_ENDPOINT}/${params.uri}`);
+    // Normalize components as an array.
+    if (params.components) {
+        params.components = Array.isArray(params.components) ? params.components : [params.components];
+        url.searchParams.set("components", params.components.join(","));
+    }
+    try {
+        return (await get({ uri: url.toString(), headers: { "X-API-Key": bungiekey }, json: true })) as T;
+    } catch (error) {
+        return error;
+    }
 }
 
 /**
  * Supported BungieMembershipType.
  */
 export const CURATED_PLATFORM: BungieMembershipType[] = [
-  BungieMembershipType.TigerBlizzard,
-  BungieMembershipType.TigerPsn,
-  BungieMembershipType.TigerXbox
+    BungieMembershipType.TigerBlizzard,
+    BungieMembershipType.TigerPsn,
+    BungieMembershipType.TigerXbox
 ];
 
 /**
@@ -44,17 +45,31 @@ export const CURATED_PLATFORM: BungieMembershipType[] = [
  * @param platform the BungieMembershipType if all's good.
  */
 export function isPlatformSupported(platform: number): boolean {
-  return CURATED_PLATFORM.includes(platform);
+    return CURATED_PLATFORM.includes(platform);
 }
 
 export function createHierarchyIfNeeded(system: System, targetDirectory: string): void {
-  try {
-    system.fs.accessSync(targetDirectory, system.fs.constants.F_OK);
-    return;
-  } catch (error) {
-    if (error.code !== "ENOENT") {
-      throw error;
+    try {
+        system.fs.accessSync(targetDirectory, system.fs.constants.F_OK);
+        return;
+    } catch (error) {
+        if (error.code !== "ENOENT") {
+            throw error;
+        }
     }
-  }
-  system.fs.mkdirSync(targetDirectory, { recursive: true });
+    system.fs.mkdirSync(targetDirectory, { recursive: true });
+}
+
+export function isRunning(win: string, mac: string, linux: string) {
+    return new Promise(function (resolve, reject) {
+        const plat = process.platform
+        const cmd = plat == 'win32' ? 'tasklist' : (plat == 'darwin' ? 'ps -ax | grep ' + mac : (plat == 'linux' ? 'ps -A' : ''))
+        const proc = plat == 'win32' ? win : (plat == 'darwin' ? mac : (plat == 'linux' ? linux : ''))
+        if (cmd === '' || proc === '') {
+            resolve(false)
+        }
+        exec(cmd, function (err: any, stdout: any, stderr: any) {
+            resolve(stdout.toLowerCase().indexOf(proc.toLowerCase()) > -1)
+        })
+    })
 }
